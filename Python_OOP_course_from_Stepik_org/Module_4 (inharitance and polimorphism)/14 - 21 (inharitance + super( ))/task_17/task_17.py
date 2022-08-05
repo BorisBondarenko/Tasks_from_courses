b53263632018e77d1aa34a83a57d3c0ba6aa1ad8
Boris Bond <87274242+BorisBondarenko@users.noreply.github.com>
2022-08-05 21:24:58 +0200

    new solved mission added

---
+++ Python_OOP_course_from_Stepik_org/Module_4 (inharitance and polimorphism)/14 - 21 (inharitance + super( ))/task_17/task_17.py
@@ -0,0 +1,33 @@
+class Router:
+    app = {}
+
+    @classmethod
+    def get(cls, path):
+        return cls.app.get(path)
+
+    @classmethod
+    def add_callback(cls, path, func):
+        cls.app[path] = func
+
+
+class Callback:
+    """This is class-decorator that give opportunity to write
+     some external func into class Router dict"""
+
+    def __init__(self, path, route_cls):
+        self.path = path
+        self.route_cls = route_cls
+
+    def __call__(self, func):
+        self.route_cls.add_callback(self.path, func)
+
+
+@Callback('/', Router)
+def index():
+    return '<h1>Главная</h1>'
+
+
+route = Router.get('/')
+if route:
+    ret = route()
+    print(ret)
