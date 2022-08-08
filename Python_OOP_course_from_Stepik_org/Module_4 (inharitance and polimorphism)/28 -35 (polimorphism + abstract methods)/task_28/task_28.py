bd6db4b2cb648baa901813e7b88fd7b90a91a5b4
Boris Bond <87274242+BorisBondarenko@users.noreply.github.com>
2022-08-08 14:28:06 +0200

    new solved mission added

---
+++ Python_OOP_course_from_Stepik_org/Module_4 (inharitance and polimorphism)/28 -35 (polimorphism + abstract methods)/task_28/task_28.py
@@ -0,0 +1,39 @@
+class Student:
+    def __init__(self, fio, group):
+        self._fio = fio
+        self._group = group
+        self._lect_marks = []
+        self._house_marks = []
+
+    def add_lect_marks(self, mark):
+        self._lect_marks.append(mark)
+
+    def add_house_marks(self, mark):
+        self._house_marks.append(mark)
+
+    def __str__(self):
+        return f"Студент {self._fio}: оценки на лекциях: {str(self._lect_marks)}; оценки за д/з: {str(self._house_marks)}"
+
+
+class Mentor:
+    def __init__(self, fio, subject):
+        self._fio = fio
+        self._subject = subject
+
+
+class Lector(Mentor):
+
+    def set_mark(self, student, mark):
+        student._lect_marks.append(mark)
+
+    def __str__(self):
+        return f"Лектор {self._fio}: предмет {self._subject}"
+
+
+class Reviewer(Mentor):
+
+    def set_mark(self, student, mark):
+        student._house_marks.append(mark)
+
+    def __str__(self):
+        return f"Эксперт {self._fio}: предмет {self._subject}"
