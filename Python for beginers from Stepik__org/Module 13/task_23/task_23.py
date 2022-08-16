d1d48239ee2f8dd758aff00fae1159e8b61d45c4
Boris Bond <87274242+BorisBondarenko@users.noreply.github.com>
2022-08-16 21:17:33 +0200

    new solved micro-task added

---
+++ Python for beginers from Stepik__org/Module 13/task_23/task_23.py
@@ -0,0 +1,13 @@
+from math import pi
+
+
+def get_circle(radius):
+    c = 2 * pi * radius
+    s = pi * radius ** 2
+    return c, s
+
+
+r = float(input())
+
+length, square = get_circle(r)
+print(length, square)
