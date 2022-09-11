1ce28da3debe8bf8df7af2e9c429b10539fff940
Boris Bond <87274242+BorisBondarenko@users.noreply.github.com>
2022-09-11 21:44:45 +0200

    new solved micro-task added

---
+++ Python for intermediate Stepik_org/Module 8 (sets)/task_25/task_25.py
@@ -0,0 +1,3 @@
+words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+
+print(*sorted(set([i.lower()[0] for i in words])))
